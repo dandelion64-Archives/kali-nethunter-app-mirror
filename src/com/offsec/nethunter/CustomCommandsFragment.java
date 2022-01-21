191df324b19cadf74ae2202cdd2ef64d13f058ff
kimoc0der <christian@aircrack-ng.org>
2022-01-21 19:48:21 +0100

    CustomCommandsFragment: Optimize imports

--- src/com/offsec/nethunter/CustomCommandsFragment.java
+++ src/com/offsec/nethunter/CustomCommandsFragment.java
@@ -17,7 +17,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.HorizontalScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
