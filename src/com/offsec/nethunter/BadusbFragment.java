9a9cea00ca423abcda55fa6467aa5ad3d97e7e4c
kimoc0der <christian@aircrack-ng.org>
2022-01-21 19:45:44 +0100

    BadUSBFragment: Fix annotation for 'menu'

--- src/com/offsec/nethunter/BadusbFragment.java
+++ src/com/offsec/nethunter/BadusbFragment.java
@@ -21,11 +21,11 @@ import com.offsec.nethunter.utils.ShellExecuter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class BadusbFragment extends Fragment {
-
     private String sourcePath;
     private static final String ARG_SECTION_NUMBER = "section_number";
     private final ShellExecuter exe = new ShellExecuter();
@@ -88,7 +88,7 @@ public class BadusbFragment extends Fragment {
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    public void onCreateOptionsMenu(@NonNull Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.badusb, menu);
         //WearOS optimisation
         final MenuItem sourceItem = menu.findItem(R.id.source_button);
